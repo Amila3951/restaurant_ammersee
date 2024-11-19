e62bb96d2622cea698cc8a720c1f1506f46a78ec
Amila Beganovic <amila.beganovic97@gmail.com>
2024-11-19 14:58:16 +0000

    Rename reservations folder

--- ammersee/settings.py
+++ ammersee/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reservations',
+    'restaurant',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ammersee.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'restaurant' / 'templates'],
+        'DIRS':  [BASE_DIR / 'restaurant' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'restaurant'
+
 urlpatterns = [
     path('make/', views.make_reservation, name='make_reservation'),
     path('menu/', views.menu, name='menu'),
     path('register/', views.register, name='register'),
     path('confirmation/', views.reservation_confirmation, name='reservation_confirmation'),
     path('my_reservations/', views.my_reservations, name='my_reservations'),
+    path('edit/<int:reservation_id>/', views.edit_reservation, name='edit_reservation'),
+    path('delete/<int:reservation_id>/', views.delete_reservation, name='delete_reservation'),
 ]
