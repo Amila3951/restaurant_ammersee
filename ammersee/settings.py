8cac40a2e1cfb1f55778382d71158488ee397692
Amila Beganovic <amila.beganovic97@gmail.com>
2024-12-02 09:02:03 +0000

    Conecting menu files

--- ammersee/settings.py
+++ ammersee/settings.py
@@ -123,6 +123,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'restaurant' / 'static',
 ]
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'restaurant/static',
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
