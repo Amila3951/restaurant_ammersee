3f703a3f1129046170c6ade3448da4293ebb3497
Amila Beganovic <amila.beganovic97@gmail.com>
2024-12-03 19:27:37 +0000

    Integrate allauth for authentication and resolve login conflict

--- ammersee/urls.py
+++ ammersee/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from restaurant import views as reservation_views
-from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', reservation_views.home, name='home'), 
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')), 
-    path('restaurant/', include('restaurant.urls', namespace='restaurant')), 
+    ## path('accounts/', include('django.contrib.auth.urls')),  
+    path('', include('restaurant.urls', namespace='restaurant')), 
     path('accounts/', include('allauth.urls')),
 ]
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,8 @@
 from . import views as reservation_views
 from django.urls import path
-from . import views 
+from . import views
 from django.contrib.auth import views as auth_views
+from .views import MyLoginView
 
 app_name = 'restaurant'
 
@@ -9,10 +10,9 @@ urlpatterns = [
     path('', reservation_views.home, name='home'),  
     path('menu/', views.menu, name='menu'),
     path('make/', views.make_reservation, name='make_reservation'),
-    path('register/', views.register, name='register'),
     path('confirmation/', views.reservation_confirmation, name='reservation_confirmation'),
     path('my_reservations/', views.my_reservations, name='my_reservations'),
     path('edit/<int:reservation_id>/', views.edit_reservation, name='edit_reservation'),
     path('delete/<int:reservation_id>/', views.delete_reservation, name='delete_reservation'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/login/', MyLoginView.as_view(), name='account_login'),
 ]
