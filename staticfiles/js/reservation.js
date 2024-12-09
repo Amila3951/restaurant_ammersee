ae1fbaad282394d012884afcf4aef3eb9e295201
Amila Beganovic <amila.beganovic97@gmail.com>
2024-12-09 18:28:53 +0000

    Add logout functionality

--- staticfiles/js/reservation.js
+++ staticfiles/js/reservation.js
@@ -1,8 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const redirectTo = urlParams.get('next');
-  
-    if (redirectTo) {
-      window.location.href = redirectTo;
-    }
-  });
+import emailjs from '@emailjs/browser';
+
+emailjs.init("YOUR_PUBLIC_KEY");
+emailjs.send("service_ab2vrqc", "template_oo7qvf5", templateParams)
+
+  .then(function (response) {
+
+    console.log("Welcome email sent successfully!", response.status, response.text);
+  }, function (error) {
+    console.log("There was an error sending the email:", error);
+   });
