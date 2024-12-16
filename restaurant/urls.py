f87e706aadfe5ca2e42955a9d5af1c905d8fa5a1
Amila Beganovic <amila.beganovic97@gmail.com>
2024-12-16 08:05:33 +0000

    Implemented an admin view to display all reservations, accessible only to superusers, with a dedicated template and a modified navbar to reflect the admin access

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,12 +1,13 @@
-from . import views as reservation_views  
+from . import views as reservation_views 
 from django.urls import path 
 from . import views 
-from django.contrib.auth import views as auth_views  
-from .views import MyLoginView  
+from django.contrib.auth import views as auth_views 
+from .views import MyLoginView   
 
 app_name = 'restaurant'  
 
 urlpatterns = [
+    path('admin/reservations/', views.admin_reservations, name='admin_reservations'),
     path('home', reservation_views.home, name='home'),  # URL for the home page
     path('menu/', views.menu, name='menu'),  # URL for the menu page
     path('make/', views.make_reservation, name='make_reservation'),  # URL for making a reservation
@@ -16,4 +17,5 @@ urlpatterns = [
     path('accounts/login/', MyLoginView.as_view(), name='account_login'),  # URL for the login page using a custom view
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='restaurant:home'), name='account_logout'),  # URL for logging out, redirects to home
     path('confirmation/<int:reservation_id>/', views.reservation_confirmation, name='reservation_confirmation'),  # URL for reservation confirmation
+    
 ]
