f8dd1755feae86d7a3b4537232cd21eeaaa3db84
Amila Beganovic <amila.beganovic97@gmail.com>
2025-01-04 20:47:29 +0000

    Configuration for Heroku deployment with Redis caching

--- ammersee/settings.py
+++ ammersee/settings.py
@@ -106,6 +106,16 @@ DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get("REDIS_URL"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
