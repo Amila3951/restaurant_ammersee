0e8a89d953d4ad7c86a51d0d8f8d6820854255c9
Amila Beganovic <amila.beganovic97@gmail.com>
2025-01-07 15:48:29 +0000

    Enhance README with header, footer, and user story details

--- ammersee/urls.py
+++ ammersee/urls.py
@@ -6,8 +6,5 @@ urlpatterns = [
     path('', reservation_views.home, name='home'), 
     path('restaurant/', include('restaurant.urls', namespace='restaurant')),
     path('admin/', admin.site.urls),
-    path('', include('restaurant.urls')),
-    path('my_reservations/', include('restaurant.urls', namespace='reservations')), 
-    path('', include('restaurant.urls', namespace='restaurant')), 
     path('accounts/', include('allauth.urls')),
 ]
